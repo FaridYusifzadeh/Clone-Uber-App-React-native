8b70908bcc8ce6a2a4530b3cca86043ad89fe90e
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-20 20:00:21 +0400

    add active button

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-export default function HeaderTabs() {
+export default function HeaderTabs(props) {
+    const [activeTab,setActiveTab] = useState("Delivery")
   return (
-    <View>
-      <Text>HeaderTabs</Text>
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        text="Delivery"
+        btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        btnColor="white"
+        textColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </View>
   );
 }
+
+const HeaderButton = (props) => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.activeTab === props.text ? "black" : "white",
+      paddingVertical: 10,
+      paddingHorizontal: 22,
+      borderRadius: 10,
+    }}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text
+      style={{
+        color: props.activeTab === props.text ? "white" : "black",
+        fontSize: 15,
+        fontWeight: "900",
+      }}
+    >
+      {props.text}
+    </Text>
+  </TouchableOpacity>
+);
