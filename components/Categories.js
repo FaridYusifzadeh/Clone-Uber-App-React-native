e4dd784825979e18f7b508ffd536a814ece255c9
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-21 14:44:46 +0400

    complete categories

--- components/Categories.js
+++ components/Categories.js
@@ -34,7 +34,14 @@ const items = [
 
 export default function Categories() {
   return (
-    <View>
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {items.map((item, index) => (
           <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
