45da7702a0d49055e1b69834710d037a1011f73b
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-21 15:11:40 +0400

    complete view resturant item

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,13 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
+
+// ******* Components ********
+
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
+import RestaurantItems from "../components/RestaurantItems";
+
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
@@ -10,7 +15,12 @@ export default function Home() {
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Categories />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems />
+        <RestaurantItems />
+      </ScrollView>
     </SafeAreaView>
   );
 }
