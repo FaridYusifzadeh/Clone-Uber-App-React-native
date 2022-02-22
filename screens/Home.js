c8aed946163797633d59eecee19bdf72f9859ff5
FaridYusifzadeh <fariddsx2015@gmail.com>
2022-02-22 14:12:04 +0400

    connect endpoint yelp api

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // ******* Components ********
 
@@ -10,8 +10,29 @@ import RestaurantItems, {
   localRestaurants,
 } from "../components/RestaurantItems";
 
+const YELP_API_KEY =
+  "H_btWYIQIF8Zv_Hz0kOszBwd0-3QBa2tLAZzyRJix_pbAXkAmiakj_5UXiSM9IkNHDMyPZOhZf5DoBeWjzyoEytSJHGCdz4GJPFJg0rwhRByYe70Z-9j2YJED7UUYnYx";
+
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("San Francisco");
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  });
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "#fff", padding: 15 }}>
